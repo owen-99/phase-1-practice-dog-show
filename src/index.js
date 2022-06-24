b81f137de6dc05e3ed9b5dd12e0ee8b8d95c045f
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-24 14:39:26 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/dogs')
+    .then(resp => resp.json())
+    .then (dogs => dogs.forEach(renderDogs))
 
 })
+
+     fetch('http://localhost:3000/dogs')
+    .then(resp => resp.json())
+    .then (dogs => dogs.forEach(renderDogs))
+
+
+function renderDogs (dog) {
+    let tableBody = document.querySelector("#table-body")
+    let newRow = document.createElement('tr')
+    let tableDataName = document.createElement('td')
+    let tableDataBreed = document.createElement('td')
+    let tableDataGender = document.createElement('td')
+    let editDog = document.createElement('button')
+
+    tableBody.append(newRow)
+    tableDataName.innerText = dog.name
+    newRow.append(tableDataName)
+    tableDataBreed.textContent = dog.breed
+    newRow.append(tableDataBreed)
+
+    tableDataGender.textContent = dog.sex
+    newRow.append(tableDataGender)
+
+    editDog.textContent = "Edit Dog"
+    newRow.append(editDog);
+
+    let newForm = document.getElementById('dog-form')
+
+    editDog.addEventListener('click', (e) =>{
+        console.log(dog.id)
+        newForm.breed.value = dog.breed
+        newForm.name.value = dog.name
+        newForm.sex.value = dog.sex
+    })
+}
+
+function handleToDo (e) {
+    console.log(e)
+
+} 
